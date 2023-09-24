5693ac5aca7a698548e6863b4323bd65a47cc1d8
Akash Chaudhary <akashchaudhary0201@gmail.com>
2023-09-24 21:28:25 +0530

    a

--- src/Routes/Notes.jsx
+++ src/Routes/Notes.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 
 function Notes() {
     return (
-        <div>
-            <h1>
-                Notes
-            </h1>
-        </div>
+        <>
+            <div className="notes_main_container">
+                <div className="notes_content">
+                    <h1>
+                        Choose Your Semester
+                    </h1>
+                    <div className="semester">
+                        <button className='sem_btn'>
+                            <span>Semester 1</span>
+                        </button>
+                        <button className='sem_btn'>
+                            <span>Semester 2</span>
+                        </button>
+                    </div>
+                    <div className="semester">
+                        <button className='sem_btn'>
+                            <span>Semester 3</span>
+                        </button>
+                        <button className='sem_btn'>
+                            <span>Semester 4</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
