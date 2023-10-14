ad35c32cef7a7ef6764e500c558f61a6bc1e112e
Akash Chaudhary <akashchaudhary0201@gmail.com>
2023-10-14 15:28:47 +0530

    Created  Team's component

--- src/Routes/Team.jsx
+++ src/Routes/Team.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Akash from "./Images/Akash.jpeg"
+import Akash from "./Images/akash.jpg"
+import Nitish from "./Images/nitish.jpg"
 
 function Team() {
     return (
@@ -10,12 +11,28 @@ function Team() {
                         Our Team
                     </h1>
                     <div className="team_members">
-                        <div className="member1" style={{ backgroundImage: `url(${Akash})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center", width: "300px", height: "300px", borderRadius: "10px" }}>
+                        <div className="member1" style={{ backgroundImage: `url(${Akash})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center", width: "300px", height: "300px" }}>
 
                             <div className="member_info">
                                 <div className='overlay'></div>
                                 <h3>Akash</h3>
+                                <h4>Front End</h4>
+                                <ul className='ul1'>
+                                    <li><i class="fa-brands fa-linkedin"></i></li>
+                                </ul>
 
+                                <ul className='ul2'>
+                                    <li><i class="fa-brands fa-instagram"></i></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        <div className="member1" style={{ backgroundImage: `url(${Nitish})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", backgroundPosition: "center center", width: "300px", height: "300px" }}>
+
+                            <div className="member_info">
+                                <div className='overlay'></div>
+                                <h3>Nitish</h3>
+                                <h4>Backend</h4>
                                 <ul className='ul1'>
                                     <li><i class="fa-brands fa-linkedin"></i></li>
                                 </ul>
@@ -27,6 +44,7 @@ function Team() {
 
                         </div>
                     </div>
+                    <p className='opp_para'>Want to join our Team? <span>Contact Us!!</span></p>
                 </div>
             </div>
         </>
