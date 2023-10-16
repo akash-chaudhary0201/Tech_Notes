a6395aa596aca15ea8b547846f7358c45b6b8b49
Akash Chaudhary <akashchaudhary0201@gmail.com>
2023-10-16 22:05:43 +0530

    a

--- src/Routes/Subjects/Semester 1/FCET.jsx
+++ src/Routes/Subjects/Semester 1/FCET.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function FCET() {
+
+    const [pdfLink, setPdfLink] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://kiet-ten.vercel.app/api/getpdfs', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                    // Add any other headers if needed
+                }
+            });
+            const data = await response.json();
+            setPdfLink(data.link);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    // useEffect(() => {
+    //     fetch('https://kiet-ten.vercel.app/api/getpdfs', {
+    //         method: "GET",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+    //     })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setPdfLink(data.link); x
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching data:', error);
+    //         });
+    // }, []);
+
+
     return (
         <>
             <div className="subject_main_container">
                 <div className="subjects_content">
-
+                    <button>
+                        <a href={pdfLink} target='_blank'>Syllabus</a>
+                    </button>
                 </div>
             </div>
         </>
