ca2858610b0b494618dd02cd4fb405c655e54e26
Akash Chaudhary <akashchaudhary0201@gmail.com>
2023-10-20 11:16:28 +0530

    commiting to check the login and sign functionality on home page

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function Home() {
 
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
 
 
     return (
@@ -15,50 +18,49 @@ function Home() {
                     <h2 className='head_hide'>
                         Empowering <span>MCA Success</span> : Notes, Discussions, Insights
                     </h2>
-                    <div className="moving_text">
-                        <h2>
+                    {
+                        isAuthenticated ? (
+                            <div className="btns">
+                                <NavLink to="/notes"><button className='home_btn'>
+                                    <span class="circle1"></span>
+                                    <span class="circle2"></span>
+                                    <span class="circle3"></span>
+                                    <span class="circle4"></span>
+                                    <span class="circle5"></span>
+                                    <span class="text">Notes</span>
+                                </button></NavLink>
+                                <NavLink to="/pyq"><button className='home_btn'>
+                                    <span class="circle1"></span>
+                                    <span class="circle2"></span>
+                                    <span class="circle3"></span>
+                                    <span class="circle4"></span>
+                                    <span class="circle5"></span>
+                                    <span class="text">PYQs</span>
+                                </button></NavLink>
+                                <NavLink to="/vam"><button className='home_btn'>
+                                    <span class="circle1"></span>
+                                    <span class="circle2"></span>
+                                    <span class="circle3"></span>
+                                    <span class="circle4"></span>
+                                    <span class="circle5"></span>
+                                    <span class="text">VAM</span>
+                                </button></NavLink>
+                                <NavLink to="/discussion"><button className='home_btn'>
+                                    <span class="circle1"></span>
+                                    <span class="circle2"></span>
+                                    <span class="circle3"></span>
+                                    <span class="circle4"></span>
+                                    <span class="circle5"></span>
+                                    <span class="text">Discussion</span>
+                                </button></NavLink>
+                            </div>
+                        ) : (
+                            <li>
+                                <button className='log__btn' onClick={() => loginWithRedirect()}><i className="fa-solid fa-user"></i></button>
+                            </li>
+                        )
+                    }
 
-                        </h2>
-                    </div>
-                    <div className="btns">
-
-                        <NavLink to="/notes"><button className='home_btn'>
-                            <span class="circle1"></span>
-                            <span class="circle2"></span>
-                            <span class="circle3"></span>
-                            <span class="circle4"></span>
-                            <span class="circle5"></span>
-                            <span class="text">Notes</span>
-                        </button></NavLink>
-
-                        <NavLink to="/pyq"><button className='home_btn'>
-                            <span class="circle1"></span>
-                            <span class="circle2"></span>
-                            <span class="circle3"></span>
-                            <span class="circle4"></span>
-                            <span class="circle5"></span>
-                            <span class="text">PYQs</span>
-                        </button></NavLink>
-
-                        <NavLink to="/vam"><button className='home_btn'>
-                            <span class="circle1"></span>
-                            <span class="circle2"></span>
-                            <span class="circle3"></span>
-                            <span class="circle4"></span>
-                            <span class="circle5"></span>
-                            <span class="text">VAM</span>
-                        </button></NavLink>
-
-                        <NavLink to="/discussion"><button className='home_btn'>
-                            <span class="circle1"></span>
-                            <span class="circle2"></span>
-                            <span class="circle3"></span>
-                            <span class="circle4"></span>
-                            <span class="circle5"></span>
-                            <span class="text">Discussion</span>
-                        </button></NavLink>
-
-                    </div>
                     <div className="home_social_btns">
                         <ul className='social_list'>
                             <li>
